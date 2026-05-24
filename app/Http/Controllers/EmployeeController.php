32bc0ac6f396c650922fb705b89f87932c272584
Yekchey Flores <yekcheyf@gmail.com>
2026-05-24 13:48:36 +0800

    ok na

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+class EmployeeController extends Controller
+{
+    public function index () {
+        $employee = Employee::all();
+        return view ('employees.index', compact('employees'));
+    
+    }
+    
+    public function create () {
+        return view ('employees.create');
+        
+    }
+
+    public function store (Request $request) {
+        Employee::create([
+            'fname' => $request->fname,
+            'lname' => $request->lname,
+            'mname' => $request->mname,
+            'age' => $request->age,
+            'zipcode' => $request->zipcode,
+            'address' => $request->address
+        ]);
+        return redirect()->route('employees.index');
+    }
+
+    public function edit ($id) {
+        $employee = Employee::find($id);
+        return view ('employees.edit', compact('employees'));
+    }
+    public function update (Request $request, $id) {
+        $employee = Employee::find($id);
+        $employee->update([
+            'fname' => $request->fname,
+            'lname' => $request->lname,
+            'mname' => $request->mname,
+            'age' => $request->age,
+            'zipcode' => $request->zipcode,
+            'address' => $request->address
+        ]);
+        return redirect()->route('employees.index');
+    }
+
+    public function destroy ($id) {
+        $employee = Employee::find($id);
+        $employee->delete();
+        return redirect()->route('employees.index');
+    }
+ 
+}
